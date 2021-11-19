340a52621204ea9ef77d8c077b5e3188be7afb9f
Alex Cwik <alexcwik1490@gmail.com>
2021-11-19 13:06:11 -0600

    fixed typo in server file and added routes and models folders

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.join());
+app.use(express.json());
 
 app.use(express.static("public"));
 
